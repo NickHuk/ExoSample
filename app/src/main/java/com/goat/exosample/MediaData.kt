5cd4b78344bd66edf743a4d77314641015d3c58b
NickHuk <mykola.huk@uptech.team>
2022-08-08 16:27:04 +0300

    removed unneeded classes

--- app/src/main/java/com/goat/exosample/MediaData.kt
+++
@@ -1,6 +0,0 @@
-package com.goat.exosample
-
-data class MediaData(
-  val url: String,
-  val playbackPosition: Long = 0L
-)
--- app/src/main/java/com/goat/exosample/PackageConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.goat.exosample
-
-import com.airbnb.epoxy.PackageModelViewConfig
-
-@PackageModelViewConfig(rClass = R::class)
-interface PackageConfig
